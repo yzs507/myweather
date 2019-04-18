3fc8bbd03c73ae6d6fa7dc24b64bb90fbd41cbad
fish <fish507@qq.com>
2019-04-18 15:05:59 +0800

    加入数据库和表的各项配置.

---
+++ app/src/main/java/com/example/administrator/myweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.administrator.myweather.db;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * Created by Administrator on 2019-04-18.
+ */
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
